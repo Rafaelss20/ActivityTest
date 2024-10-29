76bdb03c204e079cfd5581351d6890d5f130894a
JefersonBLuz <jefersonbrluz@gmail.com>
2024-10-29 20:41:18 -0300

    Funções de senha

---
+++ src/utils/CreateUserFunctions.ts
@@ -0,0 +1,32 @@
+//Senha
+//Verificar se a senha é identica a confirmação de senha
+function veriftPassword(password: string, passwordConfirmation: string): boolean {
+    return password === passwordConfirmation
+}
+//Verificar se a senha contém número
+function hasNumber(password: string): boolean {
+    return /\d/.test(password)
+}
+//Verificar se a senha contém letra maíscula
+function hasUpperCase(password: string): boolean {
+    return /\[A-Z]/.test(password)
+}
+//Verificar se a senha contém letra minuscula
+function hasLowerCase(password: string): boolean {
+    return /\[a-z]/.test(password)
+}
+//Verificar se a senha contém nó mínimo 8 caracter.
+function hasEightChar(password: string): boolean {
+    return password.length >= 8
+}
+// async function verificarEmailExiste(email: string, ): boolean {
+//     try {
+//         const user = await User.findOne({ email });
+//         return !!user; // Retorna true se o usuário foi encontrado, false caso contrário
+//     } catch (error) {
+//         console.error('Erro ao verificar email:', error);
+//         return false;
+//     }
+// }
+
+export { hasEightChar, hasLowerCase, hasNumber, hasUpperCase, veriftPassword }
