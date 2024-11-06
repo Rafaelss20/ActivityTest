bc1e47ac493c154131859c1663f713bf097d730a
Rafael <rafael.s.santos6@ba.estudante.senai.br>
2024-11-06 16:43:14 -0300

    Criação de teste unitário com jest e testes de sistema com Cypress

--- cypress/e2e/ActivityTest.cy.js
+++ cypress/e2e/ActivityTest.cy.js
@@ -1,21 +1,62 @@
 describe('ActivityTest', () => {
-beforeEach(() => {
-  cy.visit('http://localhost:3000')
+
+
+it('Testando formulário de cadastro', () => {
+    cy.visit('http://localhost:8082/create')
+    cy.get('#firstName').type("João")
+    cy.get('#lastName').type("Silva")
+    cy.get('#email').type('joao@email.com')
+    cy.get('#password').type('Joao1234')
+    cy.get('#confirmPassword').type('Joao1234')
+    cy.get('#cpf').type('000.000.000-00')
+    cy.get('#nascimento').type('2024-11-06')
+    cy.get('#numberCard').type('1')
+    cy.get('#cvv').type('1')
+    cy.get('#dateValed').type('01/01/01')
+    cy.get('[type="submit"]').click()
+    cy.get('h1').should('be.visible')
+     })
+
+it('Testando formulário de cadastro com senha incorreta', () => {
+      cy.visit('http://localhost:8082/create')
+      cy.get('#firstName').type("João")
+      cy.get('#lastName').type("Silva")
+      cy.get('#email').type('joao@email.com')
+      cy.get('#password').type('Joao1234')
+      cy.get('#confirmPassword').type('Joao123')
+      cy.get('#cpf').type('000.000.000-00')
+      cy.get('#nascimento').type('2024-11-06')
+      cy.get('#numberCard').type('1')
+      cy.get('#cvv').type('1')
+      cy.get('#dateValed').type('01/01/01')
+      cy.get('[type="submit"]').click()
+      cy.get(':nth-child(26)').should('be.visible').and('contain', "Senha não são identicas!")
+ })
+
+it ('Testando login', () => {
+  cy.visit("http://localhost:8082/")
+  cy.get('#email').type('joao@email.com')
+  cy.get('#password').type('Joao1234')
+  cy.get('[type="submit"]').click()
+  
 })
 
+it('Testando login com email ou senha incorretos', () => {
+  cy.visit("http://localhost:8082/")
+  cy.get('#email').type('joao@email.com')
+  cy.get('#password').type('Joao123')
+  cy.get('[type="submit"]').click()
+  cy.get('li').should('be.visible').and('contain', "E-mail/Senha incorretos!")
+})
 
- it('Cenários de teste com Cypress', () => {
-  cy.get('#email').type("rafael@email.com")
-  cy.get('[type="password"]').type('Rafael123')
-  cy.get('[type="button"] > a').click()
-  cy.get('#firstName').type("Rafael")
-  cy.get('#lastName').type("Santana")
-  cy.get('#email').type('rafael@email.com')
-  cy.get('#password').type('Rafael123')
-  cy.get('#confirmPassword').type('Rafael123')
-  cy.get('#cpf').type('000.000.000-00')
-  cy.get('button').click()
-
-
-  })
+it ('Testando procura por produtos', () => {
+  cy.visit("http://localhost:8082/home")
+  cy.get('#searchProduct').type('monitor')
+  cy.get('#basic-addon1').click()
+  cy.get(':nth-child(1) > .container').should('be.visible')
+  cy.get('#searchProduct').clear()
+  cy.get('#searchProduct').type('mouse')
+  cy.get('#basic-addon1').click()
+  cy.get(':nth-child(1) > .container > p').should('be.visible')
+})
 })
